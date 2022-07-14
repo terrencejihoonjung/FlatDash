5aeed2344933457b0f8961019e373569a13d9f41
Mark <marksreese@gmail.com>
2022-07-13 18:15:52 -0700

    fixed OrderMenu bugs

--- src/components/OrderMenu.js
+++ src/components/OrderMenu.js
@@ -1,14 +1,22 @@
 import { FormControl, FormLabel, Checkbox } from "@chakra-ui/react"
+import { useState } from "react"
 
 function OrderMenu() {
+
+    const [delivery, setDelivery] = useState(true)
+
+    const handleCheck = () => {
+        setDelivery(currentDelivery => !currentDelivery)
+    }
 
     return (
         <FormControl id="orderDishesForm" >
             <FormLabel htmlFor="delivery" marginTop="1rem" >Delivery or Pick-up?</FormLabel>
             <Checkbox
             id="delivery"
-            onChange={handleInput}
-            value={form.delivery} >
+            onChange={handleCheck}
+            value={delivery}
+            defaultChecked >
                 Delivery
             </Checkbox>
         </FormControl>
@@ -16,4 +24,4 @@ function OrderMenu() {
 }
 
 
-default export OrderMenu
+export default OrderMenu
